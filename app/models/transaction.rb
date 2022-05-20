17c9cd6d72e18732f8a420234e5e4b8b2346d949
stshayna@gmail.com <stshayna@gmail.com>
2022-05-20 15:55:47 -0400

    adding 'User' as class_name to giver and receiver

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -11,7 +11,7 @@
 #
 class Transaction < ApplicationRecord
   belongs_to :item
-  belongs_to :giver
-  belongs_to :receiver
+  belongs_to :giver, class_name: "User"
+  belongs_to :receiver, class_name: "User"
   validates_presence_of :giver_id, :item_id, :receiver_id
 end
