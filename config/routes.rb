8ae02616ce9b896f49317a73653c508258da89f5
Denzel Braithwaite <braithwaite.d@hotmail.com>
2022-05-19 20:52:21 -0400

    added few comments

--- config/routes.rb
+++ config/routes.rb
@@ -2,16 +2,21 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
 
-  # Show a food item
+  # CRUD actions / routes for items
   resources :items
 
+  # Shows user's food/ingredient items
   get "my-items", to: "items#my_items"
+
+  # Edits status of food/ingredient item
   patch "my-items/set_status", to: "items#set_status", as: :set_item_status
 
+  # Creates chatroom for giver and receiver to send messages
   resources :chatrooms, only: %i[show index] do
     post "messages", to: "messages#create"
   end
 
+  # Creates feedback for the end of messages between users.
   resources :messages, only: %i[new create] do
     get "feedbacks/new", to: "feedbacks#new"
     post "feedbacks", to: "feedbacks#create"
