b1ce082178d42377064ebba904ac10769e605f3c
Denzel Braithwaite <braithwaite.d@hotmail.com>
2022-05-19 20:51:03 -0400

    working on seed

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "pages#home"
-
-  # Show a food item
-  resources :items
-
-  get "my-items", to: "items#my_items"
-  patch "my-items/set_status", to: "items#set_status", as: :set_item_status
-
-  resources :chatrooms, only: %i[show index] do
-    post "messages", to: "messages#create"
-  end
-
-  resources :messages, only: %i[new create] do
-    get "feedbacks/new", to: "feedbacks#new"
-    post "feedbacks", to: "feedbacks#create"
-  end
-
-  resources :profiles, only: %i[show edit update]
+  root to: 'pages#home'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
