00c3843ed85e47cba3d8d23ed2865acfe0ca75b2
stshayna@gmail.com <stshayna@gmail.com>
2022-05-24 19:07:05 -0400

    creating chatrooms#show page - issue with simple_form to create a message

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,2 +1,19 @@
 class MessagesController < ApplicationController
+  def create
+    @chatroom = Chatroom.find(params[:chatroom_id])
+    @message = Message.new(message_params)
+    @message.user = current_user
+    @message.chatroom = @chatroom
+    if @message.save
+      redirect_to chatroom_path(@chatroom, anchor: "message-#{@message.id}")
+    else
+      render 'chatrooms/show'
+    end
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:content)
+  end
 end
