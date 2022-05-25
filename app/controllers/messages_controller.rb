68228bde43870800dd8b02511b6c07651cbd24d5
stshayna@gmail.com <stshayna@gmail.com>
2022-05-25 18:49:55 -0400

    real-time chatroom messages

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -5,7 +5,12 @@ class MessagesController < ApplicationController
     @message.user = current_user
     @message.chatroom = @chatroom
     if @message.save
-      ActionCable.server.broadcast("everyone", "We are watching you@@")
+      ChatroomChannel.broadcast_to(
+        @chatroom,
+        message: render_to_string(partial: "message", locals: { message: @message }),
+        sender_id: @message.user.id
+      )
+      head :ok
     else
       render 'chatrooms/show'
     end
--- app/javascript/controllers/chatroom_subscription_controller.js
+++ app/javascript/controllers/chatroom_subscription_controller.js
@@ -2,14 +2,43 @@ import { Controller } from "@hotwired/stimulus"
 import consumer from "../channels/consumer"
 
 export default class extends Controller {
-  static values = { chatroomId: Number }
+  static values = { chatroomId: Number, currentUserId: Number }
+  static targets = [ "messages" ]
 
   connect() {
     // subscribe to the chatroom channel (app/channels/chatroom_channel.rb)
     this.channel = consumer.subscriptions.create(
       { channel: "ChatroomChannel", id: this.chatroomIdValue },
-      { received: data => console.log(data) }
+      { received: data => this.#insertMessageAndScrollDown(data) }
       )
-    // console.log(`I need to subscribe to the chatroom with the id ${this.chatroomIdValue}.`);
+      // console.log(`I need to subscribe to the chatroom with the id ${this.chatroomIdValue}.`);
+    }
+
+  #insertMessageAndScrollDown(data) {
+    // Logic to know if the sender is the current_user
+    const currentUser = this.currentUserIdValue === data.sender_id
+    // Creating the whole message from the `data.message` String
+    const messageElement = this.#buildMessageElement(currentUser, data.message)
+
+    this.messagesTarget.insertAdjacentHTML("beforeend", messageElement)
+    this.messagesTarget.scrollTo(0, this.messagesTarget.scrollHeight)
+  }
+
+  #buildMessageElement(currentUser, message) {
+    return `
+     <div class="message-row d-flex ${this.#justifyClass(currentUser)}">
+      <div class="${this.#userStyleClass(currentUser)}">
+        ${message}
+      </div>
+    </div>
+    `
+  }
+
+  #justifyClass(currentUser) {
+    return currentUser ? 'justify-content-end' : 'justify-content-start'
+  }
+
+  #userStyleClass(currentUser) {
+    return currentUser ? 'sender-style' : 'receiver-style'
   }
 }
