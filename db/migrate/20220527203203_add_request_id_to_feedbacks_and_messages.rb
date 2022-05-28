bfad534d53c3407654ecc6a408d4d5237d8439ae
stshayna@gmail.com <stshayna@gmail.com>
2022-05-28 18:14:38 -0400

    fix request.messages, null: false

--- db/migrate/20220527203203_add_request_id_to_feedbacks_and_messages.rb
+++ db/migrate/20220527203203_add_request_id_to_feedbacks_and_messages.rb
@@ -1,6 +1,6 @@
 class AddRequestIdToFeedbacksAndMessages < ActiveRecord::Migration[6.1]
   def change
-    add_reference :feedbacks, :request, foreign_key: true
+    add_reference :feedbacks, :request, foreign_key: true, null: false
     add_reference :messages, :request, foreign_key: true, null: false
   end
 end
