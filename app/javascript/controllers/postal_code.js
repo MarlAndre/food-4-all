b753eeb1994c6ae239a09859f8b2be66debef573
Marlene Andre <andre.marlene1990@gmail.com>
2022-05-28 17:33:52 -0400

    adding logic in the controller

--- app/javascript/controllers/postal_code.js
+++ app/javascript/controllers/postal_code.js
@@ -1,7 +1,7 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  connect() {
-    console.log("hello from navbar_controller!")
+  closeBtnPopup() {
+    this.parentElement.parentElement.parentElement.style.display='none';
   }
 }
