acf1f147e4bc1345d61ee7fccf5b24dd38767125
stshayna@gmail.com <stshayna@gmail.com>
2022-05-28 18:43:59 -0400

    remove clg

--- app/javascript/controllers/request_subscription_controller.js
+++ app/javascript/controllers/request_subscription_controller.js
@@ -44,7 +44,6 @@ export default class extends Controller {
 
   // To reset the form after the message has been sent:
   resetForm(event) {
-    console.log("test");
     event.target.reset()
   }
 
