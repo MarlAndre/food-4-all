2a2e35c08baadc68954fa6bcd94a01b1cbf18a48
stshayna@gmail.com <stshayna@gmail.com>
2022-05-31 15:31:01 -0400

    migration to AddTimeZoneToUsers

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_28_185754) do
+ActiveRecord::Schema.define(version: 2022_05_31_184620) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -132,6 +132,7 @@ ActiveRecord::Schema.define(version: 2022_05_28_185754) do
     t.string "address"
     t.float "latitude"
     t.float "longitude"
+    t.string "time_zone", default: "Eastern Time (US & Canada)"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
