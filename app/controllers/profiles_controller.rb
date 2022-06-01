8890c888191af8df7dbc22e28cce414ea0e271e7
stshayna@gmail.com <stshayna@gmail.com>
2022-06-01 16:52:26 -0400

    added a 'my profile' page, linked in the dropdown menu of the navbar too

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,2 +1,17 @@
 class ProfilesController < ApplicationController
+  before_action :find_profile
+  def show
+    verify_current_user
+  end
+
+  private
+
+  def find_profile
+    @profile = User.find(params[:id])
+  end
+
+  # current user can only visit his/her own profile page.
+  def verify_current_user
+    redirect_to profile_path(current_user) unless @profile == current_user
+  end
 end
