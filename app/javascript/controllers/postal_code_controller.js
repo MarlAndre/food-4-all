ae485e1530ca17c8d51b0d00615bbe1641e1f0a4
Marlene Andre <andre.marlene1990@gmail.com>
2022-06-04 14:19:23 -0400

    popup appears one per session and we can click cards after submiting address

--- app/javascript/controllers/postal_code_controller.js
+++ app/javascript/controllers/postal_code_controller.js
@@ -3,7 +3,7 @@ import { csrfToken } from "@rails/ujs"
 
 
 export default class extends Controller {
-  static targets = [ "input", "submit" ]
+  static targets = [ "input", "submit", "popup" ]
 
   closeBtnPopup() {
     this.element.style.display='none';
@@ -12,10 +12,10 @@ export default class extends Controller {
 
   connect() {
     this.inputTarget.value = ''
-    window.localStorage.setItem('name', 'Marlene');
+    this.showPopupOnLoad()
   }
 
- // Find a way to send params (form input) to fetch.
+  // Will show results when we send our location
   submitForm(event) {
     event.preventDefault()
 
@@ -27,14 +27,12 @@ export default class extends Controller {
       .then(response => response.json())
 
       .then((items) => {
-
-        console.log("items", items)
         const cardsItems = document.querySelector("#cards-items");
         cardsItems.innerHTML='';
 
         items.forEach((item) => {
           console.log(item)
-          const itemCard = `<div class="card-product">
+          const itemCard = `<div class="card-product" data-id="${item[0].id}">
               <img src="https://raw.githubusercontent.com/lewagon/fullstack-images/master/uikit/skateboard.jpg" />
               <div class="card-product-infos">
                 <h2>${item[0].name}</h2>
@@ -44,14 +42,27 @@ export default class extends Controller {
               </div>
           </div>`
           cardsItems.insertAdjacentHTML("beforeend", itemCard)
+
         })
+        const cards = document.querySelectorAll(".card-product")
+        cards.forEach(card => {
+          card.addEventListener("click", () =>{
+            window.location.href = window.location.origin + "/items/" + card.dataset.id
+          })
+        })
+
       })
       .catch(e => console.log('error', e.message))
 
       .finally(() => {
         this.element.style.display='none';
       })
-
   }
-
+  // show PopUp once per session
+  showPopupOnLoad() {
+    if (!localStorage.getItem('isPopUpSeen')) {
+        this.popupTarget.style.display = 'flex'
+        localStorage.setItem('isPopUpSeen', 'true')
+    }
+  }
 }
