1f60793637be1d06a0bd7ee0a1a6318b16dac7ff
stshayna@gmail.com <stshayna@gmail.com>
2022-06-04 17:31:31 -0400

    my favorites toggle

--- app/javascript/controllers/toggle_favorite_controller.js
+++ app/javascript/controllers/toggle_favorite_controller.js
@@ -1,14 +1,11 @@
 import { Controller } from "@hotwired/stimulus"
+import ContextExclusionPlugin from "webpack/lib/ContextExclusionPlugin";
 
 export default class extends Controller {
   static targets = [ "icon" ]
 
   red(event) {
-    if (this.iconTarget.classList.contains('red')) {
-      this.iconTarget.classList.remove('red');
-    } else {
-      this.iconTarget.classList.add('red');
-    }
-    console.log(this.iconTarget)
+    console.log("click");
+    this.iconTarget.querySelector('i').classList.toggle('red')
   }
 }
