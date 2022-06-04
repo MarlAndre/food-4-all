dd6f797f4592d0c48669ae1e79b9d20a31a988de
Denzel Braithwaite <braithwaite.d@hotmail.com>
2022-06-04 12:11:18 -0400

    removed lat and lon from items. User will use User.address for Geocoder

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_01_205755) do
+ActiveRecord::Schema.define(version: 2022_06_04_160852) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -93,8 +93,6 @@ ActiveRecord::Schema.define(version: 2022_06_01_205755) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "name"
-    t.float "latitude"
-    t.float "longitude"
     t.index ["user_id"], name: "index_items_on_user_id"
   end
 
