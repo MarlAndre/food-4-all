3411c3b22b7faf91b9684a45f61d1be977ee4037
stshayna@gmail.com <stshayna@gmail.com>
2022-06-04 14:27:11 -0400

    stimulus action

--- app/javascript/controllers/toggle_favorite_controller.js
+++ app/javascript/controllers/toggle_favorite_controller.js
@@ -2,6 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   static values = { itemId: Number }
+  static targets = [ "icon" ]
 
   connect () {
     console.log(this.itemIdValue);
