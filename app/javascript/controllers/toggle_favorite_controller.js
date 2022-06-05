5bb78e79c771671639dc0d7cb7006a311d01bdbc
stshayna@gmail.com <stshayna@gmail.com>
2022-06-04 22:06:45 -0400

    'heart an item' is working

--- app/javascript/controllers/toggle_favorite_controller.js
+++ app/javascript/controllers/toggle_favorite_controller.js
@@ -5,7 +5,6 @@ export default class extends Controller {
   static targets = [ "icon" ]
 
   red(event) {
-    console.log("click");
     this.iconTarget.querySelector('i').classList.toggle('red')
   }
 }
