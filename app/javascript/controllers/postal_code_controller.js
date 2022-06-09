1c8f0bb11fdab9ab76c52e27c40f7486be1c3e32
Marlene Andre <andre.marlene1990@gmail.com>
2022-06-08 20:44:21 -0400

    trying to fix json

--- app/javascript/controllers/postal_code_controller.js
+++ app/javascript/controllers/postal_code_controller.js
@@ -18,7 +18,6 @@ export default class extends Controller {
   // Will show results when we send our location
   submitForm(event) {
     event.preventDefault()
-    window.location.href="items";
 
     fetch(`items?${new URLSearchParams({ postal_code: this.inputTarget.value })}`,{
       method: "GET",
@@ -31,7 +30,6 @@ export default class extends Controller {
         const cardsItems = document.querySelector("#cards-items");
         cardsItems.innerHTML='';
 
-        // This needs to be updated
         items.forEach((item) => {
           const itemCard = `<div class="card-product" data-id="${item[0].id}">
               <img src="https://images.unsplash.com/photo-1554136383-fa88b2d86aaf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=928&q=80" />
@@ -58,6 +56,7 @@ export default class extends Controller {
       .finally(() => {
         this.element.style.display='none';
       })
+    // window.location.href="items";
   }
   // show PopUp once per session
   showPopupOnLoad() {
