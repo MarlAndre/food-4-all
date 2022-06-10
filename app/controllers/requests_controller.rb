b8644f0acc54ce3ba6dca6c4ba2946c89e11d2f7
Denzel Braithwaite <braithwaite.d@hotmail.com>
2022-06-09 20:55:42 -0400

    adds distance to chat

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -9,6 +9,8 @@ class RequestsController < ApplicationController
   # make sure only the current users can access the page, otherwise redirect to Requests#index page.
   def show
     @request = Request.find(params[:id])
+    @item = @request.item
+    @distance = @item.distance_from(current_user.address).round(1)
     if current_user == @request.giver || current_user == @request.receiver
       @message = Message.new
     else
@@ -25,6 +27,19 @@ class RequestsController < ApplicationController
       redirect_to request_path(@request)
     else
       render "items/show"
+    end
+  end
+
+  private
+
+  # Sets distance for each user that's nearby.
+  def calculate_distance
+    users = User.all
+    current_coordinates = Geocoder.coordinates(@current_postal_code)
+    users.each do |user|
+      current_coordinates
+      total_distance = user.distance_from(current_coordinates).round(1)
+      @distances_between_other_users[user.id] = total_distance
     end
   end
 end
