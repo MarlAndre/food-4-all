cee13eddd2698a2fbf1f1e7139c58e19fa5f3376
Denzel Braithwaite <braithwaite.d@hotmail.com>
2022-06-11 11:51:25 -0400

    removed buttons from profile, moved chat info to my profile

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,5 +1,6 @@
 class ProfilesController < ApplicationController
   def my_profile
     @profile = User.find(current_user.id)
+    @requests = Request.where(receiver: current_user).or(Request.where(giver: current_user))
   end
 end
